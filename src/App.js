d65059ea9569e7940ccfc93813d8de684f219354
artem <amelnychenko06@gmail.com>
2022-07-03 15:12:37 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { Component } from 'react';
+import Education from './components/education';
+import General from './components/general';
+import Preview from './components/preview';
+import Work from './components/work';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <General />
+        <Education />
+        <Work />
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
